173db3c3209b7e281f7ae66c442d2a9adccd5ff3
Reid <rreid112@gmail.com>
2020-02-05 13:02:23 -0600

    Clean up ignored files

--- src/server.js
+++ src/server.js
@@ -7,10 +7,10 @@ const app = express();
 
 app.use(compression());
 
-app.use('/static', express.static('../build/static'));
+app.use('/static', express.static(process.env.ABSOLUTE_STATIC_PATH));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join('../build/index.html'));
+  res.sendFile(path.join(process.env.ABSOLUTE_STATIC_PATH, '../index.html'));
 });
 
 const PORT = 3002;
